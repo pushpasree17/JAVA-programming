2fa38da404b27b1c7c204e6481ca4b061dc64aa9
pushpasree17 <sree06262@gmail.com>
2025-12-08 18:23:19 +0530

    Add files via upload

--- example.java
+++ example.java
@@ -1,13 +1,5 @@
-import java.util.Scanner;
 public class example{
-  public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);
-    int a;
-    String name=sc.next();
-    String dept=sc.next();
-    a=sc.nextInt();
-    System.out.println("enter your name:"+name);
-    System.out.println("enter your department:"+dept);
-    System.out.println("enter your rollno:"+a);
+public static void main(String args[]){
+system.out.println("welcome to java");
   }
-}
+  }
