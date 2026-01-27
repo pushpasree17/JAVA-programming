35b54ae662b391d7838c0b04f3cf1f9ed2e6c764
pushpasree17 <sree06262@gmail.com>
2026-01-27 17:28:13 +0530

    Add files via upload

---
+++ citizen.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class student{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+     int a=sc.next();
+    System.out.println("enter your rollno:"+a);
+  }
+}
+  
