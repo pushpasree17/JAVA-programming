10c10031c806661a58db5aaf3ee081c3165707ad
pushpasree17 <sree06262@gmail.com>
2026-01-31 16:23:41 +0530

    Add files via upload

---
+++ Vote.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Vote{
+  public static void main(String[]args){
+    Scanner sc= new Scanner(System.in);
+    int age=sc.nextInt();
+    if(age>=18)
+    System.out.println("Eligible to vote");
+    else
+    System.out.println("not Eligible");
+  }
+}
