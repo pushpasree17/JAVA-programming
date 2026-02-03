0b78962eb683ac3743620f69c8b0c2de45bc2dc6
pushpasree17 <sree06262@gmail.com>
2026-02-03 15:52:36 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class student{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    String name=sc.next();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    int d=sc.nextInt();
+    int e=sc.nextInt();
+    System.out.println("enter your rollno:"+a);
+    System.out.println("enter your name:"+name);
+    System.out.println("enter maek1:"+b);
+    System.out.println("enter maek2:"+c);
+    System.out.println("enter maek3:"+d);
+    System.out.println("enter maek4:"+e);
+   }
+}
+  
