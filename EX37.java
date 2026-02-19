814fc147b2d66588f49d955e6f7abdedb5625fcd
pushpasree17 <sree06262@gmail.com>
2026-02-19 16:27:21 +0530

    Add files via upload

---
+++ EX37.java
@@ -0,0 +1,10 @@
+class EX37{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 3 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
