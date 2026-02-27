f2d2bbb7f7c7bd63777613519dd78ebdc2bb5d05
pushpasree17 <sree06262@gmail.com>
2026-02-27 17:13:39 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,17 @@
+class Sum {
+  int add(int a, int b) {
+        return a + b;
+    }
+   int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        Sum obj = new Sum();
+        System.out.println(obj.add(5, 10));       
+        System.out.println(obj.add(5, 10, 15));    
+        System.out.println(obj.add(5.5, 2.5));    
+    }
+}
